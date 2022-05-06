8c3d90d0d3134b87b7393ff4c37b551ee210e19e
Luiza <luizablaquini@hotmail.com>
2022-05-06 17:26:24 -0300

    test button ok

---
+++ src/components/Button/Button.spec.jsx
@@ -0,0 +1,38 @@
+import { fireEvent, getByRole, render, screen } from '@testing-library/react';
+//import { userEvent } from '@testing-library/user-event';
+import { Button } from '.';
+
+describe('<Button/>', () => {
+    it('should render with the text "Load More"', () => {
+        render(<Button text='Load more'/>);
+
+        expect.assertions(1); // para garantir que pelo menos uma acersao ocorreu (expect)
+                                // comum em teste assincronos
+                                
+        const button = screen.getByRole('button', {name: /load more/i});
+        expect(button).toBeInTheDocument();
+    }); 
+
+    it('should call function on button click', () => {
+        const fn = jest.fn();
+
+        render(<Button text='Load more' onClick={fn} />);
+
+        const button = screen.getByRole('button', {name: /load more/i});
+
+        //userEvent.click(button);
+        fireEvent.click(button);
+
+        expect(fn).toHaveBeenCalledTimes(1);
+    }); 
+
+    it('should be disabled when "disabled=true"', () => {
+        render(<Button text='Load more' disabled={true} />);
+        expect(screen.getByRole('button', {name: /load more/i})).toBeDisabled();
+    }); 
+
+    it('should be enabled when "disabled=false"', () => {
+        render(<Button text='Load more' disabled={false} />);
+        expect(screen.getByRole('button', {name: /load more/i})).toBeEnabled();
+    }); 
+});
--- src/templates/home/Home.test.jsx
+++ src/templates/home/Home.test.jsx
@@ -1,13 +1,5 @@
 describe('<Home/>', () => {
-    it('test one', () => {
-        expect(1).toBe(1);
-    });
-
-    it('test two', () => {
-        expect(1).toBe(1);
-    });
-
-    it('test three', () => {
+    it('is a dummy test', () => {
         expect(1).toBe(1);
     });
 });
